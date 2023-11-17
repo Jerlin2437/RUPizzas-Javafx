fbb671d72a928e2e55af47a220b153ffd7d198f5
leijason22 <leijason22@gmail.com>
2023-11-16 22:41:05 -0500

    Update SpecialtyPizzaController.java

--- src/main/java/com/example/softmeth4/SpecialtyPizzaController.java
+++ src/main/java/com/example/softmeth4/SpecialtyPizzaController.java
@@ -2,20 +2,37 @@ package com.example.softmeth4;
 
 import com.example.softmeth4.businesslogic.Order;
 import com.example.softmeth4.businesslogic.StoreOrders;
+import com.example.softmeth4.pizzas.Deluxe;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 
-public class SpecialtyPizzaController {
+import java.util.ResourceBundle;
+
+public class SpecialtyPizzaController{
     private Order order;
+    @FXML
+    private RadioButton tomato;
+    @FXML
+    private RadioButton alfredo;
     @FXML
     private CheckBox extraCheese;
     @FXML
     private CheckBox extraSauce;
 
     @FXML
-    private ComboBox<?> chooseSpecialty;
+    private RadioButton small;
     @FXML
-    private ListView<?> toppings;
+    private RadioButton medium;
+    @FXML
+    private RadioButton large;
+
+    @FXML
+    private ComboBox<String> chooseSpecialty;
+    @FXML
+    private ListView<String> toppings;
     @FXML
     private TextField price;
     @FXML
@@ -27,6 +44,34 @@ public class SpecialtyPizzaController {
     private ToggleGroup specialtySauceButtonGroup;
     public SpecialtyPizzaController(){
         order = HelloApplication.getOrder();
+    }
+
+    @FXML
+    public void initialize(){
+        chooseSpecialty.getItems().addAll("Deluxe", "Supreme", "Meatzza", "Seafood", "Pepperoni");
+        chooseSpecialty.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            //clear existing toppings in the ListView
+            toppings.getItems().clear();
+
+            if (newValue != null) {
+                if (newValue.equals("Deluxe")) {
+                    specialtySauceButtonGroup.selectToggle(tomato);
+                    toppings.getItems().addAll("Sausage", "Pepperoni", "Green Pepper", "Onion", "Mushroom");
+                } else if (newValue.equals("Supreme")) {
+                    specialtySauceButtonGroup.selectToggle(tomato);
+                    toppings.getItems().addAll("Sausage", "Pepperoni", "Green Pepper", "Onion", "Mushroom", "Ham", "Black Olive");
+                } else if (newValue.equals("Meatzza")) {
+                    specialtySauceButtonGroup.selectToggle(tomato);
+                    toppings.getItems().addAll("Sausage", "Beef", "Ham", "Pepperoni");
+                } else if (newValue.equals("Seafood")) {
+                    specialtySauceButtonGroup.selectToggle(alfredo);
+                    toppings.getItems().addAll("Shrimp", "Squid", "Crab Meat");
+                } else if (newValue.equals("Pepperoni")) {
+                    specialtySauceButtonGroup.selectToggle(tomato);
+                    toppings.getItems().addAll("Pepperoni");
+                }
+            }
+        });
     }
 
 
