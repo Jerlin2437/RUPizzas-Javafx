58ad6297e90c2a0a349998cea8723da80f339cf2
Jerlin2437 <yuenbo3@gmail.com>
2023-11-18 12:52:50 -0500

    List view

--- src/main/java/com/example/softmeth4/BuildOwnController.java
+++ src/main/java/com/example/softmeth4/BuildOwnController.java
@@ -1,9 +1,16 @@
 package com.example.softmeth4;
 
 import com.example.softmeth4.businesslogic.Order;
+import com.example.softmeth4.enums.Topping;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
 
 public class BuildOwnController {
     private Order order;
+    @FXML
+    private ListView<Topping> selectedToppings;
+    @FXML
+    private ListView<Topping> notSelectedToppings;
     public BuildOwnController(){
         order = HelloApplication.getOrder();
     }
